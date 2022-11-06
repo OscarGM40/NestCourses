cfe125375b12822ce2fd50aff7dcf2dd101d8edb
Oscar <oscargm28@hotmail.com>
2022-11-06 10:18:48 +0100

    pre-deploy 02

--- 04-teslo-shop/src/app.module.ts
+++ 04-teslo-shop/src/app.module.ts
@@ -14,25 +14,30 @@ import { MessageWsModule } from './message-ws/message-ws.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type:'postgres',
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl:
+          process.env.STAGE === 'prod' ? { rejectUnauthorized: false } : null,
+      },
+      type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      autoLoadEntities:true,
+      autoLoadEntities: true,
       // synchronize resincroniza cualquier cambio en las entidades(agregar una columna,etc),es algo que no querré en producción,pero si en desarrollo.Normalmente en prod se hacen migraciones,no se toca la db asi por asi
-      synchronize:true 
+      synchronize: true,
     }),
     ProductsModule,
     CommonModule,
     SeedModule,
     FilesModule,
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../','public')
+      rootPath: join(__dirname, '../', 'public'),
     }),
     AuthModule,
-    MessageWsModule
+    MessageWsModule,
   ],
   controllers: [],
   providers: [],
